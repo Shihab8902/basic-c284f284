554aaf70bd35ef11037f6f24c6a9b1163dfdba09
Shihab Hasan <shihabbd018@gmail.com>
2026-06-13 23:33:16 +0600

    Added new line

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@
 const App = () => {
   return (
     <div data-jc-id="Q6NCUB">
-      <h1 className="text-red-600" data-jc-id="Q6NCTE">Hello World!</h1>
+            <span className="text-base text-gray-700 dark:text-gray-300" data-jc-id="7RUWQ3">This text was added from Jellycube</span>
+<h1 className="text-red-600" data-jc-id="Q6NCTE">Hello World!</h1>
       <h1 className="text-red-600" data-jc-id="Q6NCTE">This text was added from VS Code!</h1>
     </div>
   )
