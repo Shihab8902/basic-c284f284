892e987bbb5d094cc9241cb62ebc8a888f9218a5
shihab8902 <shihabbd018@gmail.com>
2026-06-13 23:29:44 +0600

    Add additional greeting message to App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ const App = () => {
   return (
     <div data-jc-id="Q6NCUB">
       <h1 className="text-red-600" data-jc-id="Q6NCTE">Hello World!</h1>
+      <h1 className="text-red-600" data-jc-id="Q6NCTE">This text was added from VS Code!</h1>
     </div>
   )
 }
