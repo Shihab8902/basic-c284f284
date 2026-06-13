ccd937590285326eb2980fad8bc93b9f9586be4c
Shihab Hasan <shihabbd018@gmail.com>
2026-06-13 23:28:09 +0600

    Initial commit — basic

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div data-jc-id="Q6NCUB">
+      <h1 className="text-red-600" data-jc-id="Q6NCTE">Hello World!</h1>
+    </div>
+  )
+}
+
+export default App
